b6262adcdf1f15c3c776b6490fc31ade95ff3c55
Beck-Haywood <beck.haywood@students.makeschool.com>
2020-04-21 10:45:40 -0800

    dynamic content and footer

---
+++ src/Footer.js
@@ -0,0 +1,17 @@
+// src/Project.js
+
+import React from 'react'
+import './Footer.css';
+
+
+
+function Footer(props) {
+
+  return (
+    <div class='Footer'>
+      <footer>Beck Haywood copyright 2020</footer>
+    </div>
+  )
+}
+
+export default Footer
